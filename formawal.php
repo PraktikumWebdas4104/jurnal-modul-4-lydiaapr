9a8de11d7b72555302b104934bd6fd1d2a466561
lydiaapr <43522408+lydiaapr@users.noreply.github.com>
2018-09-28 09:52:36 +0700

    Update formawal.php
    Lydia aprilyanti
    6701174016

--- formawal.php
+++ formawal.php
@@ -1,5 +1,24 @@
 <?php
-
-
-
+<form method="POST" action="proses.php"></form>
+	<table>
+		<tr>
+			<td>username</td>
+			<td><input type="text" name="username"></td>
+		</tr>
+		<tr>
+			<td>password</td>
+			<td><input type="password" name="password"></td>
+		</tr>
+		<tr>
+			<td>upload foto</td>
+			<td><input type="file" name="upload foto" placeholder="upload foto"><br>
+		</tr>
+		<tr>
+			<td>hapus foto</td>
+			<td><input type="submit" name="buttons" value="delete"></td>
+		</tr>
+		<tr>
+			<td align="right"><td><input type="submit" name="submit" value="login"></td>
+		</tr>
+	</table>
 ?>
