fb4afbc56bf36ae32a1c768116e84cf5b8b9b18f
lydiaapr <43522408+lydiaapr@users.noreply.github.com>
2018-09-28 10:09:53 +0700

    Update proses.php
    Lydia aprilyanti/6701174016

--- proses.php
+++ proses.php
@@ -1,4 +1,35 @@
 <?php
+error_reporting(0);
+	$userr=$_POST['username'];
+	$passwrd=$_POST['password'];
+	$user=array("user" => "lydia",
+						"password" => "12345");
 
+	if(isset($_POST["submit"])){
+		if($userr==$user['user']&& $passwrd==$user['password']){
+			echo "login".$userr."berhasil<br><br>";
+			echo"<form method='POST'>;
+					<input type='checkbox' name='hobi[]' value='shopping'> shopping<br>
+					<input type='checkbox' name='hobi[]' value='nonton'> nonton<br>
+					<input type='checkbox' name='hobi[]' value='skating'> skating<br>
+					<input type='checkbox' name='hobi[]' value='futsal'> futsal<br><br>
+					<input type='checkbox' name='hobi[]' value='travelling'> travelling<br><br>
+					<input type='submit' name='submit2'>
+				</form>";
+		}
+		else{
+			echo "USERNAME DAN PASSWORD SALAH";
+		}
+	} 
+?>
 
+<?php 
+	if(isset($_POST['submit2'])){
+	$data = $_POST['hobi'];
+	echo "hobby :";
+	foreach ($data as $nilai) {
+		echo "$nilai,";
+	}
+	echo"<br>Gambar : <br>";
+	} 
 ?>
